0fd5f94721a2da9b5ceb056816cdc936af4fbb64
Callan Bryant <callan.bryant@gmail.com>
2012-08-28 23:23:17 +0100

    +hss server (early)

--- hss-reporter.py
+++ hss-reporter.py
@@ -1,43 +1,46 @@
 import requests
+# TODO: Connection error handling, loop that
+# TODO: Real stats
+
 import time
 import copy
 
-server = 'http://status/'
+server = 'http://localhost/'
+#server = 'http://status/'
 key    = 'grumpycheese'
 
-def init(payload):
+def update(payload,mode="update"):
+	"""Updates the server, initial or update depending on mode"""
 	payload.update({"key":key})
-	print requests.post(server+'init',data=payload).text
-
-def update(payload):
-	payload.update({"key":key})
-	print requests.post(server+'update',data=payload).text
+	print requests.post(server+mode,data=payload).text
 	
 # send some limits to define this platform
-init({
+update({
 	# memory in MB
-	"ram": 4096,
+	"memory": 4096,
 	# total storage capacity in GB
-	"hdd": 128,
+	"storage": 128,
 	# total synchronous internet bandwidth in Mbps
 	# false if this is unknown
 	"bandwidth": 200,
 	# is temperature supported? t/f (if so, updates are the highest temperature)
 	"temperature": True,
-})
+},"init")
 
 # in a loop, update the server
 while True:
 	update({
 		# memory used in MB
-		"ram": 2321,
+		"memory": 2321,
 		# total storage capacity in GB
-		"hdd": 28,
+		"storage": 28,
 		# total synchronous internet bandwidth in Mbps
 		# false if this is unknown
 		"bandwidth": 20,
 		# temp in degrees celcius
 		"temperature": 73,
+		# uptime in days
+		"uptime": 23
 	})
 	time.sleep(1)
 
---
+++ hss-server.js
@@ -0,0 +1,97 @@
+// Listens for hss-reporter clients
+
+var key = 'grumpycheese'
+
+
+var express = require('express')
+var app = express()
+var http = require('http')
+var server = http.createServer(app)
+var io = require('socket.io').listen(server)
+
+server.listen( process.env.PORT||80 )
+
+// state to be synchronised with the client
+var state = {}
+
+app.use(express.bodyParser())
+
+app.post('/init', function(req, res) {
+	var ip = socket.handshake.address.address
+	getFqdn(ip,function(host,domain) {
+		status[host] = {
+			Uptime : {
+				units : ' days'
+			},
+			Load : {
+				units : '%',
+				max : 100,
+				gradient : 'negative'
+			},
+			Storage : {
+				units : 'GB',
+				max : req.body.storage,
+				gradient : 'negative'
+			},
+			Memory : {
+				units : 'MB',
+				max : req.body.memory,
+				gradient : 'negative'
+			}
+		}
+
+		if (req.body.temperature)
+			status[host].Temperature = {
+				units : '&deg;C',
+				max : 80,
+				gradient : 'negative',
+				min : 15,
+				alarm :true
+			}
+
+		if (req.body.traffic)
+			status[host].Traffic = {
+				units : 'Mbps',
+				max : req.body.traffic,
+				gradient : 'negative'
+			}
+
+		io.sockets.emit('refresh',status)
+
+		res.json({
+			success  : "Initialised server to pool",
+			hostname : host,
+			domain   : domain
+		})
+	})
+})
+
+	
+app.post('/update', function(req, res){
+	res.end()
+})
+
+app.use(express.static(__dirname + '/www'))
+
+
+io.set('log level',1)
+io.enable('browser client minification')
+io.enable('browser client etag')
+io.enable('browser client gzip')
+io.set('transports', ['websocket','flashsocket','htmlfile','xhr-polling','jsonp-polling'])
+
+
+// longhand defining parameters of attributes
+var initial = {}
+
+// new client
+io.sockets.on('connection',function (socket){
+	socket.emit('refresh',state)
+	socket.emit('title','darksky.io server fleet')
+})
+
+
+// given an ip, callback with host and domain
+function getFqdn(ip,cb) {
+	cb(ip)
+}
