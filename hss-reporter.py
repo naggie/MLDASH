a2a05745c21e58a190b0a054673e7014e375b3f0
Callan Bryant <callan.bryant@gmail.com>
2012-08-29 01:46:40 +0100

    exception spinning

--- hss-reporter.py
+++ hss-reporter.py
@@ -13,37 +13,42 @@ def update(payload,mode="update"):
 	"""Updates the server, initial or update depending on mode"""
 	payload.update({"key":key})
 	print requests.post(server+mode,data=payload).text
-	
-# send some limits to define this platform
-update({
-	# memory in MB
-	"Memory": 4096,
-	# total storage capacity in GB
-	"Storage": 128,
-	# total synchronous internet bandwidth in Mbps
-	# false if this is unknown
-	"Traffic": 200,
-	# is temperature supported? t/f (if so, updates are the highest temperature)
-	"Temperature": True,
-},"init")
 
-# in a loop, update the server
 while True:
-	update({
-		# memory used in MB
-		"Memory": 2321,
-		# total storage capacity in GB
-		"Storage": 28,
-		# total synchronous internet bandwidth in Mbps
-		# false if this is unknown
-		"Traffic": 20,
-		# temp in degrees celcius
-		"Temperature": 73,
-		# uptime in days
-		"Uptime": 23,
-		# 0-100 CPU load 
-		"Load": 92
-	})
-	time.sleep(1)
+	try:
+		# send some limits to define this platform
+		update({
+			# memory in MB
+			"Memory": 4096,
+			# total storage capacity in GB
+			"Storage": 128,
+			# total synchronous internet bandwidth in Mbps
+			# false if this is unknown
+			"Traffic": 200,
+			# is temperature supported? t/f (if so, updates are the highest temperature)
+			"Temperature": True,
+		},"init")
 
-
+		# in a loop, update the server
+		while True:
+			update({
+				# memory used in MB
+				"Memory": 2321,
+				# total storage capacity in GB
+				"Storage": 28,
+				# total synchronous internet bandwidth in Mbps
+				# false if this is unknown
+				"Traffic": 20,
+				# temp in degrees celcius
+				"Temperature": 73,
+				# uptime in days
+				"Uptime": 23,
+				# 0-100 CPU load 
+				"Load": 92
+			})
+			time.sleep(1)
+	except:
+		print
+		print "Problem with "+server
+		print "Reconnecting in 4 seconds..."
+		time.sleep(4)
