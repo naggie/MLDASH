50ff1a0c3192101fb428a18a453be808f13799cc
Callan Bryant <callan.bryant@gmail.com>
2012-08-29 10:55:44 +0100

    +timeout

--- hss-reporter.py
+++ hss-reporter.py
@@ -13,7 +13,7 @@ key    = 'banana'
 def update(payload,mode="update"):
 	"""Updates the server, initial or update depending on mode"""
 	payload.update({"key":key})
-	req = requests.post(server+mode,data=payload)
+	req = requests.post(server+mode,data=payload,timeout=10)
 	return req
 
 while True:
